a83788306fcbe718eccf8062b6c5ab82e88c9de2
Jigyasa2408 <94893985+Jigyasa2408@users.noreply.github.com>
2022-07-24 22:46:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ st.title('Largest Number Among Three Nos.')
 a = st.number_input('Enter 1st number')
 b = st.number_input('Enter 2nd number')
 c = st.number_input('Enter 3rd number')
-if((a>b)and(a>c)):
+if((a>=b)and(a>=c)):
   result=a
   st.write(' Largest No = ', result)
-elif((b>a)and(b>c)):
+elif((b>=a)and(b>=c)):
   result=b
   st.write(' Largest No = ', result)
-elif((c>a)and(c>b)):
+elif((c>=a)and(c>=b)):
   result=c
   st.write(' Largest No = ', result)
